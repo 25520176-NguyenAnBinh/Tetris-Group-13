9777d84c3d69bbc37f6f39c0141a9f207c28ff7a
25521089DoAnhMinh <25521089@gm.uit.edu.vn>
2026-05-16 22:52:24 +0700

    Thêm tính năng Hold Piece

--- GameEngine.h
+++ GameEngine.h
@@ -13,6 +13,9 @@ private:
     Board gameBoard;
     Piece* currentPiece;
     int speed;
+    Piece* holdPiece;      
+    bool isHoldEmpty;      
+    bool canHold;
 
     // Hàm di chuyển con trỏ console (Giữ nguyên từ code gốc)
     void gotoxy(int x, int y) {
@@ -59,6 +62,27 @@ private:
                     currentPiece->rotate();
                 }
             }
+            if (c == 'c' && canHold) {
+                if (isHoldEmpty) {
+                    // Lần đầu bấm: Cất gạch hiện tại đi, đẻ ra gạch mới
+                    holdPiece = currentPiece;
+                    isHoldEmpty = false;
+                    spawnPiece();
+                }
+                else {
+                    // Đã có gạch: Hoán đổi 2 con trỏ cho nhau
+                    Piece* temp = currentPiece;
+                    currentPiece = holdPiece;
+                    holdPiece = temp;
+
+                    // Đặt lại tọa độ cho gạch vừa lấy ra từ Hold
+                    currentPiece->x = 4;
+                    currentPiece->y = 0;
+                }
+
+                // Khóa lại, không cho đổi liên tục trong lúc đang rơi nữa
+                canHold = false;
+            }
             if (c == 'q') exit(0); // Thoát game ngay lập tức như code gốc
         }
     }
@@ -98,6 +122,9 @@ public:
     GameEngine() {
         speed = 200; // Giữ nguyên tốc độ gốc
         currentPiece = nullptr;
+        holdPiece = nullptr;
+        isHoldEmpty = true;
+        canHold = true;
 
         // Ẩn con trỏ chuột
         HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -109,6 +136,7 @@ public:
 
     ~GameEngine() {
         if (currentPiece != nullptr) delete currentPiece;
+        if (holdPiece != nullptr) delete holdPiece;
     }
 
     // Hàm thay thế cho int main() cũ
@@ -134,6 +162,7 @@ public:
                 // Giải phóng RAM của viên gạch cũ trước khi tạo viên mới
                 delete currentPiece;
                 spawnPiece();
+                canHold = true;
             }
 
             draw();
