c705378af307fae1f7c56f79d159c03120da01f8
Đỗ Tuấn Đạt <25520263@gm.uit.edu.vn>
2026-05-16 17:59:36 +0700

    sửa lại Piece.h
    Refactor Piece class methods and member variables for clarity.

--- Piece.h
+++ Piece.h
@@ -1,31 +1,22 @@
 #pragma once
 #include <iostream>
-
 using namespace std;
 
 #define H 20
 #define W 15
 
-class Piece
-{
+class Piece {
 protected:
     char matrix[4][4];
     char type;
 
 public:
-    int x;
-    int y;
+    int x;             // Tọa độ x hiện tại trên bảng
+    int y;             // Tọa độ y hiện tại trên bảng
 
-public:
-    Piece();
-
-    virtual ~Piece();
-    virtual void rotateBlocks();
-    void copyBlocks(const char block[4][4]);
-    void boardDelBlock(char board[][W]);
-    void block2Board(char board[][W]);
-    bool canMove(char board[][W], int dx, int dy);
-    bool canRotate(char board[][W]);
-    char getCell(int i, int j) const;
-    char getType() const;
+    Piece()
+    virtual ~Piece() 
+    virtual void rotate() 
+    char getCell(int , int ) const
+    char getType() const 
 };
